abc20e27082143dfbc35710004d24d339ca08af2
Thanatmet-Arcadia <monchai.t@arcadiaapm.com>
2023-08-21 16:23:34 +0700

    refactor: change dummy data to call the API with firebase.

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,19 +1,42 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-import { getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from '../../helpers/api-util';
 
 import EventList from '../../components/events/event-list'
 import ResultsTitle from '../../components/events/results-title'
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function FilteredEventsPage() {
+function FilteredEventsPage(props) {
+    const [loadedEvents, setLoadedEvents] = useState();
     const router = useRouter();
 
     const filterData = router.query.slug;
 
-    if (!filterData) {
+    // client side data fetching
+    const fetcher = (url) => fetch(url).then((res) => res.json());
+    const { data, error } = useSWR(
+        "https://nextjs-course-7b2cb-default-rtdb.firebaseio.com/events.json",
+        fetcher
+    );
+
+    useEffect(() => {
+        if (data) {
+            const events = [];
+        
+            for (const key in data) {
+                events.push({
+                    id: key,
+                    ...data[key]
+                });
+            }
+            setLoadedEvents(events);
+        }
+    }, [data]);
+
+    if (!loadedEvents) {
         return <p className="center">Loading....</p>
     }
 
@@ -23,12 +46,18 @@ function FilteredEventsPage() {
     const numYear = +filteredYear;
     const numMonth = +filteredMonth;
 
-    if (isNaN(numYear) ||
+    const filteredEvents = loadedEvents.filter((event) => {
+        const eventDate = new Date(event.date);
+        return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+      });
+
+      if (isNaN(numYear) ||
         isNaN(numMonth) ||
         numYear > 2023 || 
         numYear < 2021 ||
         numMonth < 1 ||
-        numMonth > 12 
+        numMonth > 12 ||
+        error
     ) {
         return  <Fragment>
         <ErrorAlert>
@@ -41,11 +70,6 @@ function FilteredEventsPage() {
         </div>
         </Fragment>;
     }
-
-    const filteredEvents = getFilteredEvents({
-        year: numYear,
-        month: numMonth,
-    });
 
     if (!filteredEvents || filteredEvents.length === 0) {
         return <Fragment>
@@ -69,5 +93,48 @@ function FilteredEventsPage() {
         </Fragment>
     )
 }
+
+// export async function getServerSideProps(context) {
+//     const { params } = context; 
+
+//     const filterData = params.slug;
+
+//     const filteredYear = filterData[0];
+//     const filteredMonth = filterData[1];
+
+//     const numYear = +filteredYear;
+//     const numMonth = +filteredMonth;
+
+//     if (isNaN(numYear) ||
+//         isNaN(numMonth) ||
+//         numYear > 2023 || 
+//         numYear < 2021 ||
+//         numMonth < 1 ||
+//         numMonth > 12 
+//     ) {
+//         return {
+//             props: { hasError: true },
+//             // notFound: true,
+//             // redirect: {
+//             //     destination: '/error'
+//             // }
+//         }
+//     }
+
+//     const filteredEvents = await getFilteredEvents({
+//         year: numYear,
+//         month: numMonth,
+//     });
+
+//     return {
+//         props: {
+//             events: filteredEvents,
+//             date: {
+//                 year: numYear,
+//                 month: numMonth
+//             }
+//         },
+//     }
+// }
 
 export default FilteredEventsPage;
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,22 +1,19 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-    const router = useRouter();
-    console.log(router);
-    const eventId = router.query.eventId;
-    const event = getEventById(eventId);
+function EventDetailPage(props) {
+
+    const event = props.selectedEvent;
 
     if (!event) {
-        return <ErrorAlert>
-            <p>No event found!</p>
-        </ErrorAlert>
+        return <div className="center">
+            <p>Loading...</p>
+        </div>
     }
 
     return (
@@ -28,6 +25,36 @@ function EventDetailPage() {
             </EventContent>
         </Fragment>
     )
+}
+
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+
+    const event = await getEventById(eventId);
+
+    if (!event) {
+        return {
+            notFound: true,
+        };
+    }
+
+    return {
+        props: {
+            selectedEvent: event,
+        },
+        revalidate: 30,
+    }
+}
+
+export async function getStaticPaths() {
+    const events = await getFeaturedEvents();
+
+    const paths = events.map(event => ({ params: { eventId: event.id } } ));
+
+    return {
+        paths: paths,
+        fallback: "blocking",
+    }
 }
 
 export default EventDetailPage;
